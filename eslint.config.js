7b233c61aa464b363bbb26d1163097371673464d
Dan Gowans <dangowans@users.noreply.github.com>
2024-10-23 13:01:08 -0400

    update linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,6 @@
 import eslintConfigCityssm, { tseslint } from 'eslint-config-cityssm';
 const config = tseslint.config(...eslintConfigCityssm, {
     rules: {
-        '@cspell/spellchecker': [
-            'warn',
-            {
-                cspell: {
-                    words: ['cityssm', 'nvarchar', 'recordset', 'tseslint']
-                }
-            }
-        ],
         '@typescript-eslint/no-unsafe-assignment': 'off',
         '@typescript-eslint/no-unsafe-argument': 'off',
         '@typescript-eslint/no-unsafe-call': 'off',
--- eslint.config.ts
+++ eslint.config.ts
@@ -5,14 +5,6 @@ import eslintConfigCityssm, {
 
 const config = tseslint.config(...eslintConfigCityssm, {
   rules: {
-    '@cspell/spellchecker': [
-      'warn',
-      {
-        cspell: {
-          words: ['cityssm', 'nvarchar', 'recordset', 'tseslint']
-        }
-      }
-    ],
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/no-unsafe-argument': 'off',
     '@typescript-eslint/no-unsafe-call': 'off',
